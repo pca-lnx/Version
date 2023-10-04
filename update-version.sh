5f149b298d30bc6576931327770759943c9b411e
pca-lnx <130362367+pca-lnx@users.noreply.github.com>
2023-10-04 18:55:31 +0700

    Update update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 version=$1
-jsonPath=$2
+lnxVersion=$2
+jsonPath=$3
 
 jq ".RedundantController.Version = \"$version\"" $jsonPath > "temp.json" && mv "temp.json" $jsonPath
+jq ".LNX.Version = \"$lnxVersion\"" $jsonPath > "temp.json" && mv "temp.json" $jsonPath
